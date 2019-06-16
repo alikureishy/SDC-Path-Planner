89570a777302288a4da7ae7e87294f51bca5c20f
Safdar Kureishy <safdar.kureishy@gmail.com>
2019-06-16 12:32:34 -0700

    Comment cleanup

--- src/auto/pathplanner.h
+++ src/auto/pathplanner.h
@@ -193,37 +193,4 @@ private:
     WorldMap world_map;
 };
 
-//class StraightPathPlanner : public PathPlanner {
-//public:
-//        StraightPathPlanner(double speed, int numIntervals, WorldMap_ map) {   // # of 0.2 second intervals
-//            this->speed = speed;
-//            this->numIntervals = numIntervals;
-//        }
-//        virtual tuple<vector<double>, vector<double>> plan(int targetLane, double targetVelocity) {
-//            vector<double> next_xs;
-//            vector<double> next_ys;
-//            double dist_inc = 0.5;
-//            for(int i = 0; i < this->numIntervals; i++) {
-//                double next_s = car_s + (i+1) * dist_inc;
-//                double next_d = 6; // Center of middle-lane
-//                vector<double> xy = getXY(next_s, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
-//
-//                next_xs.push_back(xy[0]);
-//                next_ys.push_back(xy[1]);
-//            }
-//            return make_tuple(next_xs, next_ys);
-//        }
-//
-//private:
-//        double speed;
-//        int numIntervals;
-//};
-//
-//class CircularPathPlanner : public PathPlanner {
-//    public:
-//
-//
-//};
-
-
 #endif
